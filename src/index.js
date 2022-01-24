86c04130d69c86b2381efd795b7048daec8568dc
orla90 <orla90@inbox.ru>
2022-01-24 22:25:57 +0300

    feat: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let nString = String(n);
+
+  for (let i = nString.length - 1; i >= 0; i -= 1) {
+      if (nString[i] !== '-') {
+        result = `${result}${nString[i]}`;
+      }     
+  }
+
+  return Number(result);
 }
